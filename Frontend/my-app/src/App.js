17a59adb53a2691c32778222e35050af2694d80a
Dhaval Agnani <dhavalagnani2684@gmail.com>
2025-01-16 21:44:54 +0530

    basic frontend

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} /> {/* Redirect to Home by default */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
